8d119c226faa72d8ab67613f5d34e41c5bb0db51
rohanbhangui <bhanguirohan@gmail.com>
2015-03-09 14:01:38 -0400

    updated countdown

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 jQuery(function($){
 	$('#donation-countdown, #donation-countdown2').countdown('2015/03/15').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
-	    + '<span class="time-item">%-w</span> week%!w '
+	    // + '<span class="time-item">%-w</span> week%!w '
 	    + '<span class="time-item">%-d</span> day%!d '
 	    + '<span class="time-item">%H</span> hr '
 	    + '<span class="time-item">%M</span> min '
